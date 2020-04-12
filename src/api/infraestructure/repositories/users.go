44dbfb6cfb8dc02208e87a72de48e940ee84bbcc
sebastianmurdoch <sebastianmurdoch12@gmail.com>
2020-04-12 19:00:12 -0300

    Fix change to pointer at query

--- src/api/infraestructure/repositories/users.go
+++ src/api/infraestructure/repositories/users.go
@@ -16,7 +16,7 @@ func NewUserRepositoryImpl(db *sqlx.DB) *UserRepositoryImpl{
 
 func (r *UserRepositoryImpl) FindAll() ([]users.User, error) {
 	allUsers := []users.User{}
-	err := r.DB.Select(allUsers, "SELECT * FROM users")
+	err := r.DB.Select(&allUsers, "SELECT * FROM users")
 	if err != nil {
 		return nil, errors.New("Error at UserRepositoryImpl-FindAll -- "+err.Error())
 	}
