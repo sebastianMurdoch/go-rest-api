ce552563b28081a4676f5b2577118069b51974e2
sebastianmurdoch <sebastianmurdoch12@gmail.com>
2020-04-12 18:53:37 -0300

    Se agrega enpoint /users

--- src/api/infraestructure/http/routes.go
+++ src/api/infraestructure/http/routes.go
@@ -9,6 +9,7 @@ func mapRoutes(app *App)  {
 
 	app.engine.GET("/ping", app.statusHandler.Ping)
 	app.engine.GET("/users", app.usersHandler.Get)
+	app.engine.POST("/users", app.usersHandler.Post)
 
 	log.Println("URL Mapping: done")
 }
