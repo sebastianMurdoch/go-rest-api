f10f32994324e50cd0929cecc60ed7ef4ef54886
sebastianmurdoch <sebastianmurdoch12@gmail.com>
2020-04-13 06:18:42 -0300

    Fix set the newrelic app name properly

--- src/api/infraestructure/http/newrelic.go
+++ src/api/infraestructure/http/newrelic.go
@@ -7,7 +7,7 @@ import (
 )
 
 func rewrelic(app *App) {
-	newRelicApp, err := newrelic.NewApplication(newrelic.Config{AppName:"go-rest-api", License:os.Getenv("NEW_RELIC_LICENSE_KEY")})
+	newRelicApp, err := newrelic.NewApplication(newrelic.Config{AppName: "safe-taiga-63543", License: os.Getenv("NEW_RELIC_LICENSE_KEY")})
 	if err != nil {
 		log.Fatal("Could not init rewrelic agent")
 	}
