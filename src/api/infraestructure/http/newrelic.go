0f31bb04dde68259ca3b1579b357b32f863a7c47
sebastianmurdoch <sebastianmurdoch12@gmail.com>
2020-04-13 07:20:12 -0300

    Add enable newrelic

--- src/api/infraestructure/http/newrelic.go
+++ src/api/infraestructure/http/newrelic.go
@@ -7,7 +7,10 @@ import (
 )
 
 func rewrelic(app *App) {
-	newRelicApp, err := newrelic.NewApplication(newrelic.Config{AppName: "safe-taiga-63543", License: os.Getenv("NEW_RELIC_LICENSE_KEY")})
+	newRelicApp, err := newrelic.NewApplication(
+		newrelic.Config{AppName: "safe-taiga-63543",
+			License: os.Getenv("NEW_RELIC_LICENSE_KEY"),
+			Enabled:true})
 	if err != nil {
 		log.Fatal("Could not init rewrelic agent")
 	}
